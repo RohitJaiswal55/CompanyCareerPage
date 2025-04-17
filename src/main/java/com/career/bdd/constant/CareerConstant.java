de632738f33d38561487116f3a9f8099f131901c
RohitJaiswal55 <rohit.jaiswal55@gmail.com>
2025-04-17 22:31:27 +0530

    adding cross browser methods

--- src/main/java/com/career/bdd/constant/CareerConstant.java
+++ src/main/java/com/career/bdd/constant/CareerConstant.java
@@ -6,6 +6,7 @@ public class CareerConstant {
 	public static final int ONE = 1;
 	public static final int ZERO = 0;
 	public static final String WHAT_IS_YOUR_AVAIABILITY = "what is your availability";
-	
+	public static final String WEBDRIVER = "WebDriver";
+	public static final String CHROME = "chrome";
 
 }
--- src/main/java/com/career/bdd/helper/CareerHelper.java
+++ src/main/java/com/career/bdd/helper/CareerHelper.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 import com.career.bdd.constant.CareerConstant;
 import com.career.bdd.pages.CareerPage;
 import com.career.bdd.utils.ConfigReader;
@@ -14,30 +17,48 @@ import io.cucumber.java.Before;
 import org.testng.asserts.SoftAssert;
 
 public class CareerHelper {
-	private WebDriver driver; // Ensuring WebDriver is initialized properly
+	private WebDriver driver;
 	private CareerPage careerPage;
-	CareerConstant CareerConstant;
-
-	SoftAssert softAssert = new SoftAssert();
+	private CareerConstant careerConstant; 
+	private SoftAssert softAssert = new SoftAssert();
+	private ConfigReader config = new ConfigReader();
+	private String browser;
 
 	@Before
 	public void setup() {
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure");
-		options.addArguments("--disable-blink-features=BlockCredentialedSubresources");
-		String projectPath = System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver", projectPath + "/src/test/resources/driver/chromedriver.exe");
-		
-		driver = new ChromeDriver(options);
+		browser = config.getProperty(CareerConstant.WEBDRIVER); 
+
+		if (browser.equalsIgnoreCase(CareerConstant.CHROME)) {
+			System.out.println("browser--->" + browser);
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure");
+			options.addArguments("--disable-blink-features=BlockCredentialedSubresources");
+
+			String projectPath = System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", projectPath + "/src/test/resources/driver/chromedriver.exe");
+
+			driver = new ChromeDriver(options);
+		} else {
+			System.out.println("browser--->" + browser);
+			EdgeOptions options = new EdgeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--no-sandbox");
+
+			String projectPath = System.getProperty("user.dir");
+			System.setProperty("webdriver.edge.driver", projectPath + "/src/test/resources/driver/msedgedriver.exe");
+
+			driver = new EdgeDriver(options);
+		}
+
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
-		
-
-		careerPage = new CareerPage(driver); // Ensure CareerPage is initialized after WebDriver
+		careerPage = new CareerPage(driver);
 	}
+
 
 	@After
 	public void teardown() {
@@ -51,8 +72,8 @@ public class CareerHelper {
 	 * @throws InterruptedException
 	 */
 	public void launchCareerPage() throws InterruptedException {
-		ConfigReader config = new ConfigReader();
-		String baseURL = config.getProperty(CareerConstant.BASE_URL); // Directly fetching baseURL key
+		
+		String baseURL = config.getProperty(CareerConstant.BASE_URL);
 
 		if (driver == null) {
 			throw new IllegalStateException("WebDriver is not initialized!");
